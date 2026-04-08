13517c3e5a65dc3eeb8b7bca7ea2ab8f0d56e102
PedroLung <pguedeslsilva@gmail.com>
2026-04-08 11:56:13 -0300

    verificar history

--- allure.config.js
+++ allure.config.js
@@ -2,6 +2,6 @@ export default {
   output: "allure-report",
   results: "allure-results",
   clean: true,
-  historyPath: "./history.jsonl", // Ele vai ler o arquivo que o YAML baixou/criou
+  historyPath: "./history.jsonl",
   appendHistory: true,
 };
