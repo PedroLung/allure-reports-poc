908df88ba5ff346b1ad29f43b3c0bc5c3d259032
PedroLung <pguedeslsilva@gmail.com>
2026-04-08 11:54:46 -0300

    subpasta history

--- allure.config.js
+++ allure.config.js
@@ -2,6 +2,6 @@ export default {
   output: "allure-report",
   results: "allure-results",
   clean: true,
-  historyPath: "./history.jsonl", // Ele vai ler o arquivo que baixamos no primeiro step do YAML
+  historyPath: "./history.jsonl", // Ele vai ler o arquivo que o YAML baixou/criou
   appendHistory: true,
 };
